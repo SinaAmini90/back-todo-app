90d0ffbc06fa74927d315412c0f36137d3a731b7
sina <siyam1990@gmail.com>
2024-07-21 17:53:45 +0330

    insert hash functions creator and validator

---
+++ src/core/utils/encryption/index.js
@@ -0,0 +1,24 @@
+import bcrypt from "bcrypt";
+import { BCRYPT_CONFIG } from "../../configs/index.js";
+
+const saltRounds = BCRYPT_CONFIG.rounds;
+
+async function hashCreator(input) {
+  try {
+    const hash = await bcrypt.hash(input, saltRounds);
+    return hash;
+  } catch (error) {
+    return error.message;
+  }
+}
+
+async function hashValidator(input, hash) {
+  try {
+    const result = await bcrypt.compare(input, hash);
+    return result;
+  } catch (error) {
+    return error.message;
+  }
+}
+
+export { hashCreator, hashValidator };
