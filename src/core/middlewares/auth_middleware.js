bdb796247d781db5c2d0874ec2fd0e9421617789
sina <siyam1990@gmail.com>
2024-08-09 16:26:02 +0330

    add auth middleware file with  auth middleware  function

---
+++ src/core/middlewares/auth_middleware.js
@@ -0,0 +1,31 @@
+import { jwtValidator } from "../auth/jwt-auth";
+
+const authMiddleware = async (req, res, next) => {
+  const authHeader = req.headers.Authorization;
+  if (!authHeader) {
+    return res.status(401).json({
+      message: "authorization header is missing!",
+    });
+  }
+  if (!authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({
+      message: "Authorization header is not in the correct format!",
+    });
+  }
+  const jwtToken = authHeader.split(" ")[1];
+  if (!jwtToken) {
+    res.status(401).json({
+      message: "JWT Token is missing!",
+    });
+  }
+  try {
+    const tokenData = await jwtValidator(jwtToken);
+    req.token = tokenData;
+    next();
+  } catch (error) {
+    res.status(401).json({
+      message: "invalid JWT Token!",
+    });
+  }
+};
+export { authMiddleware };
